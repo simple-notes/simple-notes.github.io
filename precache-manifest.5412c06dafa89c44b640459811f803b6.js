86c7a8180f3c389ded506254e07a62eb2b98d3bb
Roman Khaliulin <Roman.Khaliulin@itechart-group.com>
2019-12-03 15:42:49 +0300

    Updates

---
+++ precache-manifest.5412c06dafa89c44b640459811f803b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b88f9282d3a61be774e801bfc25212",
+    "url": "/library/index.html"
+  },
+  {
+    "revision": "c43c867cc561df27bbff",
+    "url": "/library/static/js/2.a91bd0db.chunk.js"
+  },
+  {
+    "revision": "27be2dcbe4f31ef5c01c",
+    "url": "/library/static/js/main.0fdf7da5.chunk.js"
+  },
+  {
+    "revision": "3908f78a7606c3e7dfb8",
+    "url": "/library/static/js/runtime~main.1021f0b9.js"
+  }
+]);
--- src/components/NamespaceList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function NamespaceList() {
-  return (
-    <></>
-  );
-};
-
-export default NamespaceList;
