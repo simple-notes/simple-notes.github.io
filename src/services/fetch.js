b73eb5fd22d824fe9b78fdc64cd96b2eeb1f2eb9
Roman Khaliulin <Roman.Khaliulin@itechart-group.com>
2019-12-05 17:12:37 +0300

    return fetch.js

---
+++ src/services/fetch.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { getToken, signIn } from './auth';
+import { googleApiBaseUrl } from '../config';
+export const fetch = async (addUrl, options, tryCount = 0) => {
+  if (tryCount === 3) {
+    throw new Error('Request rejected three times');
+  } 
+  tryCount += 1;
+  const token = getToken();
+  if (token) {
+    try {
+      const url = googleApiBaseUrl + addUrl;
+      const headers = {
+        'Authorization': `Bearer ${token}`
+      };
+      const { data } = await axios({
+        url,
+        headers,
+        ...options
+      });
+      return data;
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        await signIn();
+        return await fetch(addUrl, options, tryCount);
+      } else {
+        throw new Error(err.message);
+      };
+    };
+  } else {
+    await signIn();
+    return await fetch(addUrl, options, tryCount);
+  };
+};
