53d5ca694df28bbb6490d7393d463342b57971f4
Roman Khaliulin <Roman.Khaliulin@itechart-group.com>
2019-12-05 18:46:13 +0300

    Updates

---
+++ precache-manifest.e49b8c74d7099ce63b5d543559c218b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7695b534d1d221abd0277d9b9c0d729a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c43c867cc561df27bbff",
+    "url": "/static/js/2.a91bd0db.chunk.js"
+  },
+  {
+    "revision": "b0cf610d798ca3a15471",
+    "url": "/static/js/main.72887fc9.chunk.js"
+  },
+  {
+    "revision": "cd43d2e2ec9e898b943e",
+    "url": "/static/js/runtime~main.3119da2c.js"
+  }
+]);
--- src/services/events.js
+++
@@ -1,6 +0,0 @@
-import debounce from 'lodash/debounce';
-import { defaultDebounceTimeMs } from '../config'
-
-export const debounceEvent = (func, timeout = defaultDebounceTimeMs) => {
-  return debounce(func, timeout);
-};
