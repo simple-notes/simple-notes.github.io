6db451272bc7596a3e5560fbd072d901dd746eef
Roman Khaliulin <Roman.Khaliulin@itechart-group.com>
2019-12-19 18:12:38 +0300

    Updates

--- precache-manifest.0e8155c3192120d8c4aaf328b7b60478.js
+++ precache-manifest.0e8155c3192120d8c4aaf328b7b60478.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "518cdbade8bc812715a4286040e8a841",
+    "revision": "7121dea7023186e0f8df938e12009279",
     "url": "/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3c07458b.chunk.js.LICENSE"
   },
   {
-    "revision": "9587936aba5992bb2ae3",
-    "url": "/static/js/main.298dd3e5.chunk.js"
+    "revision": "3c887a28bbd63e1d420f",
+    "url": "/static/js/main.17fae9e2.chunk.js"
   },
   {
     "revision": "0324d2af77d8434a0d60",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3491fdc5b5507b54479ec019a964f100.js"
+  "/precache-manifest.0e8155c3192120d8c4aaf328b7b60478.js"
 );
 
 self.addEventListener('message', (event) => {
